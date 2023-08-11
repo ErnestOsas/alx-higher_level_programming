82f6d5a865cb9a7b72f71e977aa412f7f7fa1938
Ernest Akibor <69510862+ErnestOsas@users.noreply.github.com>
2023-08-11 18:43:22 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,25 @@
-import sys
-from calculator import add, sub, mul, div
-if __name__ == "__main__":
-    main()
-    
-def main():
-    """Calculates the result of an arithmetic expression."""
-    if len(sys.argv) != 4:
-        print("Usage: python3 calculator.py <a> <operator> <b>")
-        sys.exit(1)
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+if __name__ != "__main__":
+    exit()
 
-    try:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-    except ValueError:
-        print("Invalid number.")
-        sys.exit(1)
+argc = len(argv) - 1
+if argc != 3:
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+    exit(1)
+elif argv[2] == '+':
+    func = add
+elif argv[2] == '-':
+    func = sub
+elif argv[2] == '*':
+    func = mul
+elif argv[2] == '/':
+    func = div
+else:
+    print("Unknown operator. Available operators: +, -, *, and /")
+    exit(1)
 
-    operator = sys.argv[2]
-    if operator not in ("+", "-", "*", "/"):
-        print("Unknown operator. Available operators: +, -, *, and /")
-        sys.exit(1)
-
-    func = {"+": add, "-" : sub, "*": mul, "/" : div}[operator]
-    result = func(a, b)
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+result = func(int(argv[1]), int(argv[3]))
+print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+    argv[2], int(argv[3]), result))
