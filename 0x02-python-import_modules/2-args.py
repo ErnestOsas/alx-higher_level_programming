4287f191cc94b6a8359c54b1c61059a2271249ef
Ernest Akibor <akiborernest@gmail.com>
2023-08-11 08:10:48 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     """Print the number of and list of arguments."""
     import sys
 
-    args = sys.argv[1:] # exclude the script name
+    args = sys.argv[1:]
     count = len(args)
-    print(f"{count} argument{'s' if count != 1 else ''}.") # use f-string and conditional expression
-    for i, arg in enumerate(args, 1): # use enumerate to get index and value
-        print(f"{i}: {arg}") # use f-string
+    print(f"{count} argument{'s' if count != 1 else ''}.")
+    for i, arg in enumerate(args, 1):
+        print(f"{i}: {arg}")
