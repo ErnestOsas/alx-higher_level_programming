12418820923373e4632876c0945fb7188a4ff3f2
Ernest Akibor <akiborernest@gmail.com>
2023-09-05 21:25:05 +0000

    txt

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -8,3 +8,10 @@ def magic_string():
         magic_string.calls = 1
 
     return ', '.join(['Holberton'] * magic_string.calls)
+
+# Original solution
+#
+# #!/usr/bin/python3
+# def magic_string():
+#     setattr(magic_string, 'rep', getattr(magic_string, 'rep', -1) + 1)
+#     return 'Holberton' + ', Holberton' * getattr(magic_string, 'rep', 0)
